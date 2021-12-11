c3dbdc14762b7ff4bd178892289e879d7bff4fa2
MoreiraJorge <moreirajorge_17@outlook.pt>
2021-12-11 22:35:10 +0000

    Added recycler initial version

--- app/src/main/java/com/example/auditsec/activities/ScannerActivity.kt
+++ app/src/main/java/com/example/auditsec/activities/ScannerActivity.kt
@@ -2,31 +2,26 @@ package com.example.auditsec.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
-import android.widget.EditText
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.auditsec.adapters.ScannerRecyclerAdapter
 import com.example.auditsec.R
 import com.example.auditsec.classes.PortScan
 import com.example.auditsec.classes.ScannerItem
-import java.util.concurrent.Executors
 
 class ScannerActivity : AppCompatActivity() {
 
-    private val numThreads: Int = 16
     private val host: String = "grab.com"
     private lateinit var scanButton: Button;
     private lateinit var recyclerView: RecyclerView
     private lateinit var adapter: ScannerRecyclerAdapter
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scanner)
 
-        recyclerView = findViewById<RecyclerView>(R.id.rvScanResult)
+        recyclerView = findViewById(R.id.rvScanResult)
         scanButton = findViewById(R.id.btScan)
 
         recyclerView.layoutManager = LinearLayoutManager(this)
@@ -34,15 +29,13 @@ class ScannerActivity : AppCompatActivity() {
         recyclerView.adapter = adapter
 
         scanButton.setOnClickListener {
-            println("HERE!")
-            val executor = Executors.newFixedThreadPool(numThreads)
             val commonlyUsedPorts = intArrayOf(22, 80, 443, 3306, 21, 25, 53,1720, 8080, 8988, 9999)
+            var list = ArrayList<ScannerItem>()
             for (port in commonlyUsedPorts) {
-                val worker = PortScan(adapter ,host, port)
-                executor.execute(worker)
+                list.add(PortScan.scan(adapter ,host, port))
             }
-            executor.shutdown()
+            adapter.setList(list)
+            adapter.notifyDataSetChanged()
         }
-
     }
 }
