2ee6bdc1bd54ee94f72dc746d7e8370e72152f42
jbarbosa7 <joaquimf.barbosa1997@gmail.com>
2021-12-19 23:23:22 +0000

    Read ip and ports from user input

--- app/src/main/java/com/example/auditsec/activities/ScannerActivity.kt
+++ app/src/main/java/com/example/auditsec/activities/ScannerActivity.kt
@@ -2,7 +2,6 @@ package com.example.auditsec.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -10,39 +9,44 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.auditsec.adapters.ScannerRecyclerAdapter
 import com.example.auditsec.R
 import com.example.auditsec.classes.PortScan
-import com.example.auditsec.classes.ScannerItem
+import com.example.auditsec.classes.PortUtils
+import com.google.android.material.textfield.TextInputEditText
 import java.util.concurrent.Executors
 
 class ScannerActivity : AppCompatActivity() {
 
     private val numThreads: Int = 16
-    private val host: String = "grab.com"
-    private lateinit var scanButton: Button;
+    private lateinit var scanButton: Button
     private lateinit var recyclerView: RecyclerView
     private lateinit var adapter: ScannerRecyclerAdapter
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_scanner)
 
+        adapter = ScannerRecyclerAdapter()
         recyclerView = findViewById<RecyclerView>(R.id.rvScanResult)
-        scanButton = findViewById(R.id.btScan)
-
         recyclerView.layoutManager = LinearLayoutManager(this)
-        adapter = ScannerRecyclerAdapter();
         recyclerView.adapter = adapter
 
+        scanButton = findViewById<Button>(R.id.btScan)
+        val tiIpAddress: EditText = findViewById<EditText>(R.id.tiIpAddress)
+        val tiPorts: TextInputEditText = findViewById<TextInputEditText>(R.id.tiPorts)
+
         scanButton.setOnClickListener {
-            println("HERE!")
+            val host = tiIpAddress.text.toString()
+            val tiPortsText = tiPorts.text.toString()
+
+            val ports = PortUtils.retrievePorts(tiPortsText)
+
             val executor = Executors.newFixedThreadPool(numThreads)
             val commonlyUsedPorts = intArrayOf(22, 80, 443, 3306, 21, 25, 53,1720, 8080, 8988, 9999)
-            for (port in commonlyUsedPorts) {
+            for (port in ports) {
                 val worker = PortScan(adapter ,host, port)
                 executor.execute(worker)
             }
             executor.shutdown()
         }
-
     }
 }
---
+++ app/src/main/java/com/example/auditsec/classes/PortUtils.kt
@@ -0,0 +1,23 @@
+package com.example.auditsec.classes
+
+object PortUtils {
+    fun retrievePorts(portsString: String): List<Int> {
+        val portsList = portsString.split(',')
+        var ports: Set<Int> = mutableSetOf()
+
+        portsList.forEach {
+            val portRange = it.trim().split('-')
+            if(portRange.size == 1) {
+                ports += (portRange[0].trim().toInt())
+            } else {
+                val startPort: Int = portRange[0].trim().toInt()
+                val endPort: Int = portRange[1].trim().toInt()
+                val portsInRange = listOf(startPort..endPort).flatten()
+
+                ports += portsInRange.toSet()
+            }
+        }
+
+        return ports.toList()
+    }
+}
