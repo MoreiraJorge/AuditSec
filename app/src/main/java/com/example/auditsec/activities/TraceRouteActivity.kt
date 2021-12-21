930a44beb351cec85bb887c8124c1706638df699
MoreiraJorge <moreirajorge_17@outlook.pt>
2021-12-21 21:14:58 +0000

    Add missing import

---
+++ app/src/main/java/com/example/auditsec/activities/TraceRouteActivity.kt
@@ -0,0 +1,12 @@
+package com.example.auditsec.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.auditsec.R
+
+class TraceRouteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_trace_route)
+    }
+}
